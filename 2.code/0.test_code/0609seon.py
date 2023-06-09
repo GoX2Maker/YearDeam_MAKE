a367df404b6ed1bfc8dc2b5f8a6d03e9984ec68d
b06 <bee85919@gmail.com>
2023-06-09 20:38:34 +0900

    Fix code: 주석 추가

--- 2.code/0.test_code/0609seon.py
+++ 2.code/0.test_code/0609seon.py
@@ -5,23 +5,52 @@ import cv2
 import os
 import pandas as pd
 
-
 def input_pre2(csvPath, imgPath, moveTxt, txtFont = "NanumSquareOTF_acR", txtSize = 15):
+    
+    """prescription2 정보처리
+    prescription2.csv에 있는 빈칸 고정 위치 데이터가 moveTxt에 의해 이동된다.
+    txtFont와 txtSize도 설정값에 맞게 설정된다.
+    ==
+    Args:
+    ---
+        `csvPath [string]` : prescription2.csv 경로
+
+        `imgPath [string]` : 처방전 양식 경로
+
+        `moveTxt [int, int]` : 빈칸 고정 위치 이동
+
+        `txtFont [string]` : 글씨체
+
+        `txtSize [int]` : 글자 크기
+
+    Returns:
+    ---
+        `result_text_position [array]` : moveTxt에 의해 이동된 글자의 위치
+
+        `result_img [img]` : 글자가 삽입된 처방전 이미지.
+
+    """
     
     imgBGR = cv2.imread(imgPath)
     imgRGB = cv2.cvtColor(imgBGR, cv2.COLOR_BGR2RGB)
 
     font = ImageFont.truetype(txtFont, txtSize)
-
+    
+    # 이미지를 PIL Image 객체로 변환    
     img = Image.fromarray(imgRGB)
-
+    
+    # Draw 객체 생성
     draw = ImageDraw.Draw(img)
-
+    
+    # 'id', 'x1', 'y1', 'y2' 컬럼의 값을 이용해서 텍스트와 좌표값을 가진 딕셔너리 생성
     text_dict = {str(row['id']): (row['x1'] + moveTxt[0], row['y1'] + moveTxt[1]) for _, row in df.iterrows()}
-
+    
+    # 텍스트를 추가하는 for loop
     for text, position in text_dict.items():
+        # .text(위치, 텍스트, 텍스트 색, 폰트)
         draw.text(position, text, (0, 0, 0), font=font)
 
+    # PIL Image를 다시 numpy 배열로 변환
     img_with_text = np.array(img)
     result_img2 = cv2.cvtColor(img_with_text, cv2.COLOR_RGB2BGR)
 
