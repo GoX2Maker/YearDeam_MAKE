4e0fd507387de03f03140c4f3b3ca78bf3da07b4
GoX2 <yeesonmin@naver.com>
2023-07-02 19:51:28 +0900

    Add Code createImg메소드 기본틀 제작

--- 2.code/createDataset.py
+++ 2.code/createDataset.py
@@ -1,8 +1,8 @@
 import cv2
 import pandas as pd
 import random
-
-
+from PIL import ImageFont, ImageDraw, Image
+import numpy as np
 
 
 def CreateDataset(dbPath, medicinePath, imgPath, moveTxt = [0,0], txtFont = "malgun.ttf", txtSize = 15):
@@ -29,8 +29,8 @@ def CreateDataset(dbPath, medicinePath, imgPath, moveTxt = [0,0], txtFont = "mal
 
     """
 
+    # 처방전 데이터 생성
     data_list = []
-
     cnt = random.randint(1, 13)
     data_list += medicine(cnt, medicinePath, dbPath)
 
@@ -38,7 +38,10 @@ def CreateDataset(dbPath, medicinePath, imgPath, moveTxt = [0,0], txtFont = "mal
     data_list += injection(cnt, medicinePath, dbPath)
 
     data_list += checkSquared(dbPath)
-    print(data_list)
+
+    # 처방전 이미지 및 Json 생성
+    img, img_json  = createImg(data_list, imgPath, moveTxt, txtFont, txtSize)
+
 
 
 
@@ -154,6 +157,74 @@ def checkSquared(positionPath):
        
     return checkSquared_list
 
+
+def createImg(data, imgPath, moveTxt = [0,0], txtFont = "malgun.ttf", txtSize = 15):
+    """
+    처방전 data를 이미지로 제작
+
+    Args:
+    ---
+        `data [string]` : text위치 및 내용
+
+        `imgPath [string]` : 처방전 양식 경로
+
+        `moveTxt [int, int]` : 빈칸 고정 위치 이동
+
+        `txtFont [string]` : 글씨체
+
+        `txtSize [int]` : 글자 크기
+
+    Returns:
+    ---
+        `result_img [img]` : 글자가 삽입된 처방전 이미지.
+
+        `result_txt [json]` : 이미지에 있는 글자 위치 및 내용이 담긴 Json 파일
+
+    """
+
+    imgBGR = cv2.imread(imgPath)
+    imgRGB = cv2.cvtColor(imgBGR, cv2.COLOR_BGR2RGB)
+
+    font = ImageFont.truetype(txtFont, txtSize)
+
+    # 이미지를 PIL Image 객체로 변환
+    img = Image.fromarray(imgRGB)
+
+    # Draw 객체 생성
+    draw = ImageDraw.Draw(img)
+
+
+    text_sizes_list = []
+    # 텍스트를 추가하는 for loop    
+    for text, position in data:
+        # .text(위치, 텍스트, 텍스트 색, 폰트)
+        text_width, text_height = draw.textsize(text, font=font)
+
+        position[0] += moveTxt[0]
+        position[1] += moveTxt[1]
+
+        x = position[0]  + text_width / 2
+        y = position[1]  + text_height / 2
+        
+        # Calculate the y position to center the text
+        draw.text((x, y), text, (0, 0, 0), font=font,anchor='mm')
+        
+        text_sizes_list.append([position[0], position[1], position[0] + text_width, position[1] + text_height])
+        
+
+    # PIL Image를 다시 numpy 배열로 변환
+    img_with_text = np.array(img)
+    result_img = cv2.cvtColor(img_with_text, cv2.COLOR_RGB2BGR)
+    
+    #사각형 그리기
+    for x1, y1, x2, y2 in text_sizes_list:
+        cv2.rectangle(result_img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+
+    cv2.imshow('result', result_img)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+    return result_img
 
 
 
